a1cc04b98d5fe1fded929c710b5c153c01a478fb
Mohamad <m.h.gholikhah@gmail.com>
2022-12-24 23:59:59 +0330

    ++

--- index.php
+++ index.php
@@ -1,29 +1,23 @@
 <?php
-if (isset($_POST['numbers'])) {
-    $input = $_POST['numbers'];
-    $array = explode(',', $input);
-    function primeFactors($input)
-    {
-        while ($input % 2 == 0) {
-            echo 2, " ";
-            $input = $input / 2;
-        }
-        for ($i = 3; $i <= sqrt($input);
-             $i = $i + 2) {
-            while ($input % $i == 0) {
-                echo $i, " ";
-                $input = $input / $i;
-            }
-        }
-        if ($input > 2)
-            echo $input, " ";
+
+$input = $_POST['numbers'];
+$array = explode(',', $input);
+function primeFactors($input)
+{
+    while ($input % 2 == 0) {
+        echo 2, " ";
+        $input = $input / 2;
     }
-
-
-    primeFactors($input);
+    for ($i = 3; $i <= sqrt($input);
+         $i = $i + 2) {
+        while ($input % $i == 0) {
+            echo $i, " ";
+            $input = $input / $i;
+        }
+    }
+    if ($input > 2)
+        print_r($array);
 }
-?>
-<form method="post">
-    <label> number:</label><br/>
-    <input type="text" name="numbers"/>
-    <input type="submit" name="submit" value="run"/>
+
+
+primeFactors($input);
